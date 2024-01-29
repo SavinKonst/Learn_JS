c3f19f14bd9717dcf9215d4b7c230c1964e56181
castrosavin@gmail.com <castrosavin@gmail.com>
2024-01-29 23:22:27 +0500

    task 4. function pow(x, n)

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function min(a, b){
-  return ((a < b) ? a : b);
+function pow(x, n){
+  let result = 1;
+for (let i = 0; i < n; i++){
+  result = result * x;
 }
-alert(min(+prompt('enter a?'), +prompt('enter b?')));
+  return result;
+}
+let x = +prompt('enter x?', '');
+let n = +prompt('enter n?', '');
+if((n >= 1) && (n % 1 ==0)) {
+alert('pow(x, n) = ' + pow(x, n));}
+else alert('function supports only natural degrees');
