d4d2385c8d07cd6ca2fd5cef8900efbe071a00e2
castrosavin@gmail.com <castrosavin@gmail.com>
2024-01-30 22:31:57 +0500

    the only task

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-function pow(x, n){
-  let result = 1;
-for (let i = 0; i < n; i++){
-  result = result * x;
+function ask(quastion, yes, no){
+  if(confirm(quastion)) yes();
+  else no();
 }
-  return result;
-}
-let x = +prompt('enter x?', '');
-let n = +prompt('enter n?', '');
-if((n >= 1) && (n % 1 ==0)) {
-alert('pow(x, n) = ' + pow(x, n));}
-else alert('function supports only natural degrees');
+ask('do you agree?', ()=>alert('you are agree'), ()=>alert('you canceled'));
