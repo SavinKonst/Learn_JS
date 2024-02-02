bafc51f3e9844776dba8cb939c7a4e7265ea4d3d
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-02-02 19:51:00 +0500

    Nested describe blocks

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 describe("pow", function () {
-  function makeTest(x) {
-    let expected = x * x * x;
-    it(`${x} в степени 3 будет ${expected}`, function () {
-      assert.equal(pow(x, 3), expected);
-    });
-  }
 
-  for (let x = 1; x <= 5; x++) {
-    makeTest(x);
-  }
+  describe("возводит х в степень 3", function () {
+
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
+  });
+  
 });
