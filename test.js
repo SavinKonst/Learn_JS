6d487afca695841206c1ab748a9867041081415c
SavinKonst <castrosavin@gmail.com>
2024-02-12 18:31:41 +0500

    3_String_Task4

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-describe("truncate", function() {
-  it("усекает строку до заданной длины (включая многоточие)", function() {
-    assert.equal(
-      truncate("Вот, что мне хотелось бы сказать на эту тему:", 20),
-      "Вот, что мне хотело…"
-    );
+describe("extractCurrencyValue", function() {
+
+  it("возвращает для строки $120 число 120", function() {
+    assert.strictEqual(extractCurrencyValue('$120'), 120);
   });
 
-  it("не меняет и так короткие строки", function() {
-    assert.equal(
-      truncate("Всем привет!", 20),
-      "Всем привет!"
-    );
-  });
 
 });
