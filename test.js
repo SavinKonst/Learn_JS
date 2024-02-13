4e0fad8d6c92e43df5bc1df5e241847cd1e20793
SavinKonst <castrosavin@gmail.com>
2024-02-13 08:51:49 +0500

    4_Arrays_Task2

--- test.js
+++
@@ -1,8 +0,0 @@
-describe("extractCurrencyValue", function() {
-
-  it("возвращает для строки $120 число 120", function() {
-    assert.strictEqual(extractCurrencyValue('$120'), 120);
-  });
-
-
-});
