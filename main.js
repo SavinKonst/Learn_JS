8e3ee0cd4f0fc82a937c5cd41fee754a118d207f
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-13 22:25:49 +0500

    5.11_Date&Time_Task3

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 let date = new Date(2012, 0, 3); // 3 января 2012 года
 alert(getLocalDay(date)); // вторник, нужно показать 2
+
+function getLocalDay(date) {
+  let dotw = date.getDay();
+  return dotw == 0 ? 7 : dotw;
+}
