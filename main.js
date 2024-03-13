34676fe6cc21c88cedd45a64d026b05b36d5229c
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-13 16:44:52 +0500

    5.11_Date&Time_Task2

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let date = new Date(2012, 1, 20, 3, 12);
+let date = new Date(2012, 0, 3); // 3 января 2012 года
 
-alert(date);
+let dotw = {
+  0: "ВС",
+  1: "ПН",
+  2: "ВТ",
+  3: "СР",
+  4: "ЧТ",
+  5: "ПТ",
+  6: "СБ",
+};
+alert(getWeekDay(date)); // нужно вывести "ВТ"
+
+function getWeekDay(date) {
+  return dotw[date.getDay()];
+}
