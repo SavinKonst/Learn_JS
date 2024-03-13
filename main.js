bbf5b2872fad60c3221e9ffa682b86b0eb2757e6
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-13 08:51:19 +0500

    5.9_Object.keys,_values,_entries._Task2

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-let salaries = {
-  John: 100,
-  Pete: 300,
-  Mary: 250,
+let user = {
+  name: "John",
+  age: 30,
 };
 
-alert(sumSalaries(salaries)); // 650
-function sumSalaries(salaries) {
-  let sum = 0;
-  for (let salary of Object.values(salaries)) {
-    sum += salary;
-  }
-  return sum;
-}
+alert(count(user)); // 2
 
-// or this variant:
-/*
-function sumSalaries(salaries) {
-  return Object.values(salaries).reduce((sum, salary) => sum + salary, 0);
+function count(user) {
+  return Object.keys(user).length;
 }
-*/
