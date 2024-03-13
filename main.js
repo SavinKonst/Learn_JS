b11d7a3567602575549452a555ccc687635a1c73
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-13 16:15:12 +0500

    5.11_Date&Time_Task1

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-let salaries = {
-  John: 100,
-  Pete: 300,
-  Mary: 250,
-};
+let date = new Date(2012, 1, 20, 3, 12);
 
-alert(topSalary(salaries));
-
-function topSalary(salaries) {
-  let max = 0,
-    result = null;
-
-  for (let [name, salary] of Object.entries(salaries)) {
-    if (salary > max) {
-      max = salary;
-      result = name;
-    }
-  }
-
-  return result;
-}
+alert(date);
