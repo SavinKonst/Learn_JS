243205ab456e9db6ac12876350658445ca0dd80c
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-13 16:47:57 +0500

    5.11_Date&Time_Task3_begin

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
 let date = new Date(2012, 0, 3); // 3 января 2012 года
-
-let dotw = {
-  0: "ВС",
-  1: "ПН",
-  2: "ВТ",
-  3: "СР",
-  4: "ЧТ",
-  5: "ПТ",
-  6: "СБ",
-};
-alert(getWeekDay(date)); // нужно вывести "ВТ"
-
-function getWeekDay(date) {
-  return dotw[date.getDay()];
-}
+alert(getLocalDay(date)); // вторник, нужно показать 2
--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-describe("getWeekDay", function () {
+describe('getLocalDay возвращает "европейский" день недели', function () {
   it("3 января 2014 года - пятница", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 3)), "ПТ");
+    assert.equal(getLocalDay(new Date(2014, 0, 3)), 5);
   });
 
   it("4 января 2014 года - суббота", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 4)), "СБ");
+    assert.equal(getLocalDay(new Date(2014, 0, 4)), 6);
   });
 
   it("5 января 2014 года - воскресенье", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 5)), "ВС");
+    assert.equal(getLocalDay(new Date(2014, 0, 5)), 7);
   });
 
   it("6 января 2014 года - понедельник", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 6)), "ПН");
+    assert.equal(getLocalDay(new Date(2014, 0, 6)), 1);
   });
 
   it("7 января 2014 года - вторник", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 7)), "ВТ");
+    assert.equal(getLocalDay(new Date(2014, 0, 7)), 2);
   });
 
   it("8 января 2014 года - среда", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 8)), "СР");
+    assert.equal(getLocalDay(new Date(2014, 0, 8)), 3);
   });
 
-  it("9 января 2014 - четверг", function () {
-    assert.equal(getWeekDay(new Date(2014, 0, 9)), "ЧТ");
+  it("9 января 2014 года - четверг", function () {
+    assert.equal(getLocalDay(new Date(2014, 0, 9)), 4);
   });
 });
