a7714cb11afb8f7a5a6c9cd05de48592b2510c95
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-15 06:58:46 +0500

    5.11_Date&Time_Task5

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-let date = new Date(2015, 0, 2);
+alert(getLastDayOfMonth(2012, 1)); // 29
 
-alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
-alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
-alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
-
-function getDateAgo(base, days) {
-
-  return (new Date(+base - days*1000*60*60*24).getDate());
+function getLastDayOfMonth(year, month) {
+  let date = new Date(year, month + 1, 0);
+  return date.getDate();
 }
