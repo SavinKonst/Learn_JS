0f8a8722a8f75ce265b22948891635013bdfb3fa
SavinKonst <castrosavin@gmail.com>
2024-03-15 06:47:29 +0500

    5.11_Date&Time_Task4

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ let date = new Date(2015, 0, 2);
 alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
 alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
 alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
+
+function getDateAgo(base, days) {
+
+  return (new Date(+base - days*1000*60*60*24).getDate());
+}
