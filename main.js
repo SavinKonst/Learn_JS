105781626686e5e4b12f5fc2974b5624b1781c72
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-15 08:46:25 +0500

    5.11_Date&Time_Task8

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-alert(getSecondsToTomorrow());
+alert(formatDate(new Date(new Date() - 1))); // "прямо сейчас"
 
-function getSecondsToTomorrow() {
+alert(formatDate(new Date(new Date() - 30 * 1000))); // "30 сек. назад"
+
+alert(formatDate(new Date(new Date() - 5 * 60 * 1000))); // "5 мин. назад"
+
+// вчерашняя дата вроде 31.12.2016, 20:00
+alert(formatDate(new Date(new Date() - 86400 * 1000)));
+
+function formatDate(date) {
   let now = new Date(),
-    tommorow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-  return Math.round((tommorow - now) / 1000);
+    diff = now - date;
+  if (diff < 1000) {
+    return "прямо сейчас";
+  } else if (diff < 60000) {
+    return `${Math.round(diff / 1000)} сек. назад`;
+  } else if (diff < 3600000) {
+    return `${Math.round(diff / 60000)} мин. назад`;
+  } else
+    return `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${date
+      .getFullYear()
+      .toString()
+      .slice(-2)} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`;
+}
+
+function addZero(num) {
+  if (num >= 0 && num <= 9) {
+    return "0" + num;
+  } else {
+    return num;
+  }
 }
