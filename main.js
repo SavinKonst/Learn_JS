89fc7295c6fcd90a931f777612c891a3dea06704
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-17 19:00:36 +0500

    5.12_JSON_Task1

--- main.js
+++ main.js
@@ -1,32 +1,9 @@
-alert(formatDate(new Date(new Date() - 1))); // "прямо сейчас"
+let user = {
+  name: "Василий Иванович",
+  age: 35,
+};
 
-alert(formatDate(new Date(new Date() - 30 * 1000))); // "30 сек. назад"
+let string = JSON.stringify(user);
+let obj = JSON.parse(string);
 
-alert(formatDate(new Date(new Date() - 5 * 60 * 1000))); // "5 мин. назад"
-
-// вчерашняя дата вроде 31.12.2016, 20:00
-alert(formatDate(new Date(new Date() - 86400 * 1000)));
-
-function formatDate(date) {
-  let now = new Date(),
-    diff = now - date;
-  if (diff < 1000) {
-    return "прямо сейчас";
-  } else if (diff < 60000) {
-    return `${Math.round(diff / 1000)} сек. назад`;
-  } else if (diff < 3600000) {
-    return `${Math.round(diff / 60000)} мин. назад`;
-  } else
-    return `${addZero(date.getDate())}.${addZero(date.getMonth() + 1)}.${date
-      .getFullYear()
-      .toString()
-      .slice(-2)} ${addZero(date.getHours())}:${addZero(date.getMinutes())}`;
-}
-
-function addZero(num) {
-  if (num >= 0 && num <= 9) {
-    return "0" + num;
-  } else {
-    return num;
-  }
-}
+console.log(obj);
