f4c15772d3f5ea0439cb7db39788588b52d7d853
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-21 08:16:52 +0500

    Advanced_work_with_functions_Recursion_and_stack_Task1

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
-function sumTo(n) {
-  /*... ваш код ... */
+const num = 1000,
+  iterations = 100000;
+
+// 1. The first solution using a loop
+function iterativeSumTo(n) {
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
 }
 
-alert(sumTo(100)); // 5050
+// 2. Second solution via recursion
+function recursiveSumTo(n) {
+  if (n == 1) return n;
+  return n + recursiveSumTo(n - 1);
+}
+
+// 3. The third solution using the arithmetic progression formula n*(n+1)/2
+function gaussSumTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+// Measuring Function Execution Time
+function bench(f) {
+  let start = Date.now();
+  for (let i = 0; i < iterations; i++) f(num);
+  return Date.now() - start;
+}
+
+let time1 = 0;
+let time2 = 0;
+let time3 = 0;
+
+// bench(iterativeSumTo), bench(recursiveSumTo) и bench(gaussSumTo) поочерёдно запускаются 10 раз
+for (let i = 0; i < 10; i++) {
+  time1 += bench(iterativeSumTo);
+  time2 += bench(recursiveSumTo);
+  time3 += bench(gaussSumTo);
+}
+
+alert("Итоговое время iterativeSumTo: " + time1);
+alert("Итоговое время recursiveSumTo: " + time2);
+alert("Итоговое время gaussSumTo: " + time3);
+
