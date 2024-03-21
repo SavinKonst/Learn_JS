7ba315b077944f6e15b457a01d9c897febbfd60c
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-21 21:55:24 +0500

    Advanced_work_with_functions_Recursion_and_Stack_Task5_begin

--- main.js
+++ main.js
@@ -12,19 +12,34 @@ let list = {
   },
 };
 
-ciclePrintList(list);
+// ciclePrintList(list);
 recursivePrintList(list);
 
 function ciclePrintList(list) {
-  let copy = list;
+  let copy = list,
+    i = 1;
 
-  while (copy) {
-    alert(copy.value);
+  while (copy.next != null) {
+    i++;
     copy = copy.next;
+  }
+
+  copy = list;
+
+  for (; i != 0; i--) {
+    for (let j = 1; j < i; j++) {
+      copy = copy.next;
+    }
+    alert(copy.value);
+    copy = list;
   }
 }
 
 function recursivePrintList(list) {
-  alert(list.value);
+
   if (list.next != null) recursivePrintList(list.next);
+  else {
+    alert(list.value);
+    
+  }
 }
