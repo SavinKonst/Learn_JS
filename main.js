a1d711e62bfbcea7496bd61d621f482f113267ea
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-27 09:55:52 +0500

    Advanced_work_with_functions_Variable_scope_Closure_Task6

--- main.js
+++ main.js
@@ -1,44 +1,8 @@
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
-      },
-    },
-  },
-};
-
-ciclePrintList(list);
-recursivePrintList(list);
-
-function ciclePrintList(list) {
-  let copy = list,
-    i = 1;
-
-  while (copy.next != null) {
-    i++;
-    copy = copy.next;
-  }
-
-  copy = list;
-
-  for (; i != 0; i--) {
-    for (let j = 1; j < i; j++) {
-      copy = copy.next;
-    }
-    alert(copy.value);
-    copy = list;
-  }
+function sum(c) {
+  return function (d) {
+    return c + d;
+  };
 }
 
-function recursivePrintList(list) {
-
-  if(list.next) {
-    recursivePrintList(list.next);
-  }
-  alert(list.value);
-}
+alert(sum(1)(2));
+alert(sum(5)(-1));
