295256af8930cc4b3fcc0d58f722d512c7985968
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-29 15:31:30 +0500

    Advanced_work_with_functions_Scheduling:_setTimeout_and_setInterval_Task1_begin

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function sum(num) {
-  let res = num;
-
-  function result(number) {
-    res += number;
-    return result;
-  }
-
-  result.toString = function () {
-    return res;
-  };
-  return result;
-}
-
-alert(sum(1)(2)); // == 3; // 1 + 2
-alert(sum(1)(2)(3)); // == 6; // 1 + 2 + 3
-alert(sum(5)(-1)(2)); // == 6;
-alert(sum(6)(-1)(-2)(-3)); // == 0;
-alert(sum(0)(1)(2)(3)(4)(5)); // == 15;
+function printNumbers(from, to) {
+    
+};
