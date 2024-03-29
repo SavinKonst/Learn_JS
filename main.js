681df6b508ac686431530e0a7f9b0dfa331ae79e
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-03-29 09:03:33 +0500

    Advanced_work_with_functions_Function_object_NFE_Task2

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function sum() {
-  
+function sum(num) {
+  let res = num;
+
+  function result(number) {
+    res += number;
+    return result;
+  }
+
+  result.toString = function () {
+    return res;
+  };
+  return result;
 }
 
-sum(1)(2) == 3; // 1 + 2
-sum(1)(2)(3) == 6; // 1 + 2 + 3
-sum(5)(-1)(2) == 6;
-sum(6)(-1)(-2)(-3) == 0;
-sum(0)(1)(2)(3)(4)(5) == 15;
+alert(sum(1)(2)); // == 3; // 1 + 2
+alert(sum(1)(2)(3)); // == 6; // 1 + 2 + 3
+alert(sum(5)(-1)(2)); // == 6;
+alert(sum(6)(-1)(-2)(-3)); // == 0;
+alert(sum(0)(1)(2)(3)(4)(5)); // == 15;
