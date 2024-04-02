08595a04da7c587eb90c5fd798b67e7a133a1dc5
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-04-02 15:47:27 +0500

    Advanced_work_with_functions._Decorators_and_call_forwarding,_call/apply._Task3

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-function f(x) {
-  alert(x);
-}
-
-// создаём обёртки
-let f1000 = delay(f, 1000);
-let f1500 = delay(f, 1500);
-
-f1000("test"); // показывает "test" после 1000 мс
-f1500("test"); // показывает "test" после 1500 мс
-
-function delay(func, time) {
-  return function (...args) {
-    let savedThis = this;
-    function start() {
-      func.apply(savedThis, args);
-    }
-    setTimeout(start, time);
+function debounce(func, ms) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
   };
 }
