31e4d92f2cc7b149379284a4e722fd298d5288ef
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-04-02 15:57:49 +0500

    Advanced_work_with_functions._Decorators_and_call_forwarding,_call/apply._Task4

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-function debounce(func, ms) {
-  let timeout;
-  return function () {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, arguments), ms);
-  };
+function throttle(func, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
 }
