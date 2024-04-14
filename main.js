77c7c921c15a99ce8db56c4b8351d6e4311b9343
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-04-14 16:31:39 +0500

    Prototypes,_inheritance._Prototype_inheritance._Task2

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function askPassword(ok, fail) {
-  let password = prompt("Password?", "");
-  if (password == "rockstar") ok();
-  else fail();
-}
-
-let user = {
-  name: "John",
-
-  login(result) {
-    alert(this.name + (result ? " logged in" : " failed to log in"));
-  },
+let head = {
+  glasses: 1,
 };
 
-askPassword(user.login.bind(user, true), () => user.login(false)); // equivalent function calls
+let table = {
+  pen: 3,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+};
+
+let pockets = {
+  money: 2000,
+};
+
+table.__proto__ = head;
+bed.__proto__ = table;
+pockets.__proto__ = bed;
+
+alert(pockets.pen);
+alert(bed.glasses);
+alert(table.money);
