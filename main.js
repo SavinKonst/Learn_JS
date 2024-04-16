87279c12bdf3838d9eb2324fe4a807c11eb4b565
SavinKonst <castrosavin@gmail.com>
2024-04-16 19:05:47 +0500

    Prototypes,_inheritance._Built-in_prototypes._Task1

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-let hamster = {
-  eat(food) {
-    this.stomach.push(food);
-  },
+Function.prototype.defer = function(ms) {
+  setTimeout(this, ms);
 };
 
-let speedy = {
-  stomach: [],
+function f() {
+  alert("Hello!");
+}
 
-  __proto__: hamster,
-};
-
-let lazy = {
-  stomach: [],
-
-  __proto__: hamster,
-};
-
-// Этот хомяк нашёл еду
-speedy.eat("apple");
-alert(speedy.stomach); // apple
-
-// У этого хомяка тоже есть еда. Почему? Исправьте
-alert(lazy.stomach); // apple
+f.defer(1000); // выведет "Hello!" через 1 секунду
