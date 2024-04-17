5a8fec3a67f3ac6f32f2e8fdccad8db2479e81f3
Konstantin Savin <111475174+SavinKonst@users.noreply.github.com>
2024-04-17 16:18:10 +0500

    Prototypes,_inheritance._Built_in_prototypes._Task2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-Function.prototype.defer = function(ms) {
-  setTimeout(this, ms);
+Function.prototype.defer = function (ms) {
+  return function (a, b) {
+    setTimeout(f, ms, a, b);
+  };
 };
 
-function f() {
-  alert("Hello!");
+function f(a, b) {
+  alert(a + b);
 }
 
-f.defer(1000); // выведет "Hello!" через 1 секунду
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
