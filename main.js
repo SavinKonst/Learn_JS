6eacf7e23fa6f4c04c0383646d945fbd2a288ce5
SavinKonst <castrosavin@gmail.com>
2024-04-19 17:08:16 +0500

    Prototypes,_inheritance._Prototype_methods,_objects_without__proto__property._Task1

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-Function.prototype.defer = function (ms) {
-  return function (a, b) {
-    setTimeout(f, ms, a, b);
-  };
+let dictionary = Object.create(null);
+
+// ваш код, который добавляет метод dictionary.toString
+dictionary.toString = function() {
+  return Object.keys(this).join();
 };
 
-function f(a, b) {
-  alert(a + b);
+Object.defineProperty(dictionary, "toString", {enumerable: false});
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+  alert(key); // "apple", затем "__proto__"
 }
 
-f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__";
