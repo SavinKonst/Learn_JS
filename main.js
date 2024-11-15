c03ffe6bf4d31a92e1cf576cf29b4a043f234c6d
SavinKonst <castrosavin@gmail.com>
2024-11-16 00:40:38 +0500

    async dhowAvatar

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-fetch(`https://api.github.com/users/SavinKonst`)
-  .then(response => response.json())
-    .then(githubUser => new Promise((resolve, reject) => {
+async function showAvatar() {
+  let githubResponse = await fetch(`https://api.github.com/users/SavinKonst`);
+  let githubUser = await githubResponse.json();
       let img = document.createElement('img');
       img.src = githubUser.avatar_url;
       img.className = "promise-avatar-example";
       document.body.append(img);
   
-      setTimeout(() => {
-        img.remove();
-        resolve(githubUser);
-      }, 3000);
-    }))
-    .catch(error => alert(error.message));
+      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+      img.remove();
+      return githubUser;
+}
+  showAvatar();
+  alert('code end');
